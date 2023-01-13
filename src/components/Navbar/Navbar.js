05706e42374a0893e001b9e4e3cc1046690ff4a1
jnieto2408 <jnieto2408@gmail.com>
2023-01-12 23:12:20 -0500

    cleaning app

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -4,7 +4,7 @@ export const Navbar = () => {
     return (
         <header className="header">
             <div className="header__container">
-                <h1 className="header__logo">LOGO</h1>
+                <h1 className="header__logo">Latinows</h1>
                 <nav className="header__nav">
                     <a className="header__link" href="#">Enlace 1</a>
                     <a className="header__link" href="#">Enlace 2</a>
