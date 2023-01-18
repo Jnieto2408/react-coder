8d28ddc43d247691661786d59de4d2eb532e164d
jnieto2408 <jnieto2408@gmail.com>
2023-01-17 20:58:30 -0500

    add format to price

--- src/components/Item/Item.js
+++ src/components/Item/Item.js
@@ -4,11 +4,12 @@
 const Item = ({name, image, description, price, id}) => {
  
     return(
-            <div className="col-4">
+            <div className="col-3 m-3">
               <img src={image} alt={name} />
               <h4>{name}</h4>
               <p>{description}</p>
-              <p>Precio: <b>${price}</b></p>
+              <p>Precio: <b>{new Intl.NumberFormat("en-US", {style:"currency", currency:"USD"}).format(price)}</b></p>
+              <button className="btn btn-outline-primary">Ver mas</button>
             </div>
     )
 }
